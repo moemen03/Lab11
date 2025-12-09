0570506becffb1fe235a6e26f1762f103ab38d0b
moemen03 <moemenatia3@gmail.com>
2025-12-09 08:26:25 +0200

    second

--- angular-crud/src/main.ts
+++ angular-crud/src/main.ts
@@ -1,16 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import LogRocket from 'logrocket';
 
-LogRocket.init('xergyu/testforlab');
-LogRocket.identify('THE_USER_ID_IN_YOUR_APP', {
-  name: 'James Morrison',
-  email: 'jamesmorrison@example.com',
-
-  // Add your own custom user variables here, ie:
-  subscriptionType: 'pro'
-});
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
